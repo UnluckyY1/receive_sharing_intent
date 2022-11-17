986ffe0d307297c948f197422dbe6a3e34ef7633
Frédéric Julian <f.julian@oodrive.com>
2022-11-17 15:15:48 +0100

    fix: workaround an iOS16 crash by removing the use of groups

--- ios/Classes/SwiftReceiveSharingIntentPlugin.swift
+++ ios/Classes/SwiftReceiveSharingIntentPlugin.swift
@@ -6,9 +6,7 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
     static let kMessagesChannel = "receive_sharing_intent/messages";
     static let kEventsChannelMedia = "receive_sharing_intent/events-media";
     static let kEventsChannelLink = "receive_sharing_intent/events-text";
-    
-    private var customSchemePrefix = "ShareMedia";
-    
+        
     private var initialMedia: [SharedMediaFile]? = nil
     private var latestMedia: [SharedMediaFile]? = nil
     
@@ -53,12 +51,6 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
             result(FlutterMethodNotImplemented);
         }
     }
-
-    // By Adding bundle id to prefix, we'll ensure that the correct application will be openned
-    // - found the issue while developing multiple applications using this library, after "application(_:open:options:)" is called, the first app using this librabry (first app by bundle id alphabetically) is opened
-    public func hasMatchingSchemePrefix(url: URL?) -> Bool {
-        return url?.absoluteString.hasPrefix(self.customSchemePrefix) == true;
-    }
     
     // This is the function called on app startup with a shared link if the app had been closed already.
     // It is called as the launch process is finishing and the app is almost ready to run.
@@ -66,25 +58,13 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
     // If the URL does not include the module's prefix, we must return true since while our module cannot handle the link, other modules might be and returning false can prevent
     // them from getting the chance to.
     // Reference: https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1622921-application
-    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any] = [:]) -> Bool {
-        if let url = launchOptions[UIApplication.LaunchOptionsKey.url] as? URL {
-            if (hasMatchingSchemePrefix(url: url)) {
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if let url = launchOptions?[UIApplication.LaunchOptionsKey.url] as? URL {
+            if (url.isFileURL) {
                 return handleUrl(url: url, setInitialData: true)
             }
-            return true
-        } else if let activityDictionary = launchOptions[UIApplication.LaunchOptionsKey.userActivityDictionary] as? [AnyHashable: Any] {
-            // Handle multiple URLs shared in
-            for key in activityDictionary.keys {
-                if let userActivity = activityDictionary[key] as? NSUserActivity {
-                    if let url = userActivity.webpageURL {
-                        if (hasMatchingSchemePrefix(url: url)) {
-                            return handleUrl(url: url, setInitialData: true)
-                        }
-                        return true
-                    }
-                }
-            }
         }
+        
         return true
     }
     
@@ -94,10 +74,7 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
     // If the URL does not include the module's prefix, then we return false to indicate our module's attempt to open the resource failed and others should be allowed to.
     // Reference: https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1623112-application
     public func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        if (hasMatchingSchemePrefix(url: url)) {
-            return handleUrl(url: url, setInitialData: false)
-        }
-        return false
+        return handleUrl(url: url, setInitialData: true)
     }
     
     // This function is called by other modules like Firebase DeepLinks.
@@ -108,79 +85,23 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
     // Reference: https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1623072-application
     public func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]) -> Void) -> Bool {
         if let url = userActivity.webpageURL {
-            if (hasMatchingSchemePrefix(url: url)) {
-                return handleUrl(url: url, setInitialData: true)
-            }
+            return handleUrl(url: url, setInitialData: true)
         }
         return false
     }
     
-    private func handleUrl(url: URL?, setInitialData: Bool) -> Bool {
-        if let url = url {
-            let appGroupId = (Bundle.main.object(forInfoDictionaryKey: "AppGroupId") as? String) ?? "group.\(Bundle.main.bundleIdentifier!)"
-            let userDefaults = UserDefaults(suiteName: appGroupId)
-            if url.fragment == "media" {
-                if let key = url.host?.components(separatedBy: "=").last,
-                    let json = userDefaults?.object(forKey: key) as? Data {
-                    let sharedArray = decode(data: json)
-                    let sharedMediaFiles: [SharedMediaFile] = sharedArray.compactMap {
-                        guard let path = getAbsolutePath(for: $0.path) else {
-                            return nil
-                        }
-                        if ($0.type == .video && $0.thumbnail != nil) {
-                            let thumbnail = getAbsolutePath(for: $0.thumbnail!)
-                            return SharedMediaFile.init(path: path, thumbnail: thumbnail, duration: $0.duration, type: $0.type)
-                        } else if ($0.type == .video && $0.thumbnail == nil) {
-                            return SharedMediaFile.init(path: path, thumbnail: nil, duration: $0.duration, type: $0.type)
-                        }
-                        
-                        return SharedMediaFile.init(path: path, thumbnail: nil, duration: $0.duration, type: $0.type)
-                    }
-                    latestMedia = sharedMediaFiles
-                    if(setInitialData) {
-                        initialMedia = latestMedia
-                    }
-                    eventSinkMedia?(toJson(data: latestMedia))
-                }
-            } else if url.fragment == "file" {
-                if let key = url.host?.components(separatedBy: "=").last,
-                    let json = userDefaults?.object(forKey: key) as? Data {
-                    let sharedArray = decode(data: json)
-                    let sharedMediaFiles: [SharedMediaFile] = sharedArray.compactMap{
-                        guard let path = getAbsolutePath(for: $0.path) else {
-                            return nil
-                        }
-                        return SharedMediaFile.init(path: path, thumbnail: nil, duration: nil, type: $0.type)
-                    }
-                    latestMedia = sharedMediaFiles
-                    if(setInitialData) {
-                        initialMedia = latestMedia
-                    }
-                    eventSinkMedia?(toJson(data: latestMedia))
-                }
-            } else if url.fragment == "text" {
-                if let key = url.host?.components(separatedBy: "=").last,
-                    let sharedArray = userDefaults?.object(forKey: key) as? [String] {
-                    latestText =  sharedArray.joined(separator: ",")
-                    if(setInitialData) {
-                        initialText = latestText
-                    }
-                    eventSinkText?(latestText)
-                }
-            } else {
-                latestText = url.absoluteString
-                if(setInitialData) {
-                    initialText = latestText
-                }
-                eventSinkText?(latestText)
+    private func handleUrl(url: URL, setInitialData: Bool) -> Bool {
+        if (url.isFileURL) {
+            latestMedia = [SharedMediaFile.init(path: getAbsolutePath(for: url.path), thumbnail: nil, duration: nil, type: .file)]
+            if(setInitialData) {
+                initialMedia = latestMedia
             }
-            return true
+            eventSinkMedia?(toJson(data: latestMedia))
         }
         latestMedia = nil
         latestText = nil
         return false
     }
-    
     
     public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         if (arguments as! String? == "media") {
@@ -204,36 +125,11 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
         return nil;
     }
     
-    private func getAbsolutePath(for identifier: String) -> String? {
-        if (identifier.starts(with: "file://") || identifier.starts(with: "/var/mobile/Media") || identifier.starts(with: "/private/var/mobile")) {
+    private func getAbsolutePath(for identifier: String) -> String {
+        if (identifier.starts(with: "file://")) {
             return identifier.replacingOccurrences(of: "file://", with: "")
         }
-        let phAsset = PHAsset.fetchAssets(withLocalIdentifiers: [identifier], options: .none).firstObject
-        if(phAsset == nil) {
-            return nil
-        }
-        let (url, _) = getFullSizeImageURLAndOrientation(for: phAsset!)
-        return url
-    }
-    
-    private func getFullSizeImageURLAndOrientation(for asset: PHAsset)-> (String?, Int) {
-           var url: String? = nil
-           var orientation: Int = 0
-           let semaphore = DispatchSemaphore(value: 0)
-           let options2 = PHContentEditingInputRequestOptions()
-           options2.isNetworkAccessAllowed = true
-           asset.requestContentEditingInput(with: options2){(input, info) in
-               orientation = Int(input?.fullSizeImageOrientation ?? 0)
-               url = input?.fullSizeImageURL?.path
-               semaphore.signal()
-           }
-           semaphore.wait()
-           return (url, orientation)
-       }
-    
-    private func decode(data: Data) -> [SharedMediaFile] {
-        let encodedData = try? JSONDecoder().decode([SharedMediaFile].self, from: data)
-        return encodedData!
+        return identifier;
     }
     
     private func toJson(data: [SharedMediaFile]?) -> String? {
